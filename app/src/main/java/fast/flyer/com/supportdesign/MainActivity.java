de6f4834c5f1adfe13c08e9f8477521755029b94
liangchuanfei <liangcfly@gmail.com>
2015-11-25 21:34:53 +0800

    首页加入ActionBarDrawerToggle和ToolBar

--- app/src/main/java/fast/flyer/com/supportdesign/MainActivity.java
+++ app/src/main/java/fast/flyer/com/supportdesign/MainActivity.java
@@ -2,16 +2,18 @@ package fast.flyer.com.supportdesign;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.RelativeLayout;
-import android.widget.Toast;
+import android.widget.FrameLayout;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -22,22 +24,38 @@ public class MainActivity extends AppCompatActivity {
     NavigationView mNvMenu;
     @Bind(R.id.id_drawer_layout)
     DrawerLayout mDrawerLayout;
-    @Bind(R.id.rl_content)
-    RelativeLayout mRlContent;
+    @Bind(R.id.main_toolbar)
+    Toolbar mToolbar;
+    @Bind(R.id.main_recyler_view)
+    RecyclerView mRecylerView;
+    @Bind(R.id.fl_right)
+    FrameLayout mFlRight;
+    @Bind(R.id.main_content)
+    FrameLayout mMainContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        initViews();
         initEvent();
     }
 
+    private void initViews() {
+        mToolbar.setTitle("SupportDesign");
+        setSupportActionBar(mToolbar);
+    }
+
     private void initEvent() {
-        mDrawerLayout.setDrawerListener(new DrawerLayout.DrawerListener() {
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,mToolbar,
+                 R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close) {
+
+
             @Override
             public void onDrawerSlide(View drawerView, float slideOffset) {
-
+                super.onDrawerSlide(drawerView , slideOffset);
                 float scale = 1 - slideOffset;
                 float rightScale = 0.8f + scale * 0.2f;
 
@@ -47,42 +65,42 @@ public class MainActivity extends AppCompatActivity {
 
 //                    drawerView.setScaleX(leftScale);
 //                    drawerView.setScaleY(leftScale);
+
+                    //ViewCompat.setScaleX();
+
                     drawerView.setAlpha(0.6f + 0.4f * (1 - scale));
 
-                    mRlContent.setTranslationX(drawerView.getMeasuredWidth() * (1 - scale));
-                    mRlContent.setPivotX(0);
-                    mRlContent.setPivotY(mRlContent.getMeasuredHeight() / 2);
-                    mRlContent.setScaleX(rightScale);
-                    mRlContent.setScaleY(rightScale);
+                    mMainContent.setTranslationX(drawerView.getMeasuredWidth() * (1 - scale));
+                    mMainContent.setPivotX(0);
+                    mMainContent.setPivotY(mRecylerView.getMeasuredHeight() / 2);
+                    mMainContent.setScaleX(rightScale);
+                    mMainContent.setScaleY(rightScale);
 
                 } else {
 
-                    mRlContent.setTranslationX(-drawerView.getMeasuredWidth() * slideOffset);
-                    mRlContent.setPivotX(mRlContent.getMeasuredWidth());
-                    mRlContent.setPivotY(mRlContent.getMeasuredHeight() / 2);
+                    mMainContent.setTranslationX(-drawerView.getMeasuredWidth() * slideOffset);
+                    mMainContent.setPivotX(mRecylerView.getMeasuredWidth());
+                    mMainContent.setPivotY(mRecylerView.getMeasuredHeight() / 2);
 
-                    mRlContent.setScaleX(rightScale);
-                    mRlContent.setScaleY(rightScale);
+                    mMainContent.setScaleX(rightScale);
+                    mMainContent.setScaleY(rightScale);
 
                 }
-
             }
 
-            @Override
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+            }
+
             public void onDrawerOpened(View drawerView) {
-
+                super.onDrawerOpened(drawerView);
             }
 
-            @Override
-            public void onDrawerClosed(View drawerView) {
-
-            }
-
-            @Override
-            public void onDrawerStateChanged(int newState) {
-
-            }
-        });
+        };
+        //如果不设置导航icon不出来
+        drawerToggle.syncState();
+        //不要忘了设置
+        mDrawerLayout.setDrawerListener(drawerToggle);
 
         mNvMenu.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -90,24 +108,24 @@ public class MainActivity extends AppCompatActivity {
                 Log.i(TAG, "----id:" + menuItem.getItemId());
                 switch (menuItem.getItemId()) {
                     case R.id.toolBar:
-                        Intent intent = new Intent(MainActivity.this , ToolBarActivity.class);
+                        Intent intent = new Intent(MainActivity.this, ToolBarActivity.class);
                         startActivity(intent);
                         break;
 
                     case R.id.coordinatorLayout:
-                        Intent coordinatorLayoutIntent = new Intent(MainActivity.this , CoordinatorLayoutActivity.class);
+                        Intent coordinatorLayoutIntent = new Intent(MainActivity.this, CoordinatorLayoutActivity.class);
                         startActivity(coordinatorLayoutIntent);
                         break;
                     case R.id.appBarLayout:
 
-                        Intent appBarIntent = new Intent(MainActivity.this , AppBarLayoutActivity.class);
+                        Intent appBarIntent = new Intent(MainActivity.this, AppBarLayoutActivity.class);
                         startActivity(appBarIntent);
 
                         break;
 
                     case R.id.collapsingToolbarLayout:
 
-                        Intent coolIntent = new Intent(MainActivity.this , CollapsingToolbarLayoutActivity.class);
+                        Intent coolIntent = new Intent(MainActivity.this, CollapsingToolbarLayoutActivity.class);
                         startActivity(coolIntent);
 
                         break;
